08a4c16b3fe1fd8232bf457ef5677af586a4f0a6
Toretto99 <117114759+Toretto99@users.noreply.github.com>
2023-12-28 20:45:53 +0100

    Encrypting the credentials

--- creating_env_file.py
+++ creating_env_file.py
@@ -1,13 +1,19 @@
-# from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet
 
 
 def writing_file(file_name):
-    file = open(file_name, 'w')
-    username= input("Please enter the username: ")
-    password = input("Please enter the password: ")
+    file = open(file_name, 'wb')
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    username= bytes(input("Please enter the username: "), "utf-8") # We need the input as bytes form for the encryption
+    password = bytes(input("Please enter the password: "), 'utf-8')
     if len(username) > 0 and len(password) > 0:
-        file.writelines([username+"\n", password])
+        encrypt_username = f.encrypt(username)
+        encrypt_password = f.encrypt(password)
+        file.writelines([encrypt_username+b"\n", encrypt_password])
     file.close()
+    # if you wanna see the decrypted code, just deactivate the below print statement
+    print(f.decrypt(encrypt_username), f.decrypt(encrypt_password))
     return username, password
 
 def adding_to_ignore(name_file):
@@ -16,7 +22,7 @@ def adding_to_ignore(name_file):
     if name_file not in reading:
         f.write(name_file)
     
-    
+
 file_name = "credentials.dev"
-# writing_file(file_name)
+writing_file(file_name)
 adding_to_ignore(file_name)
